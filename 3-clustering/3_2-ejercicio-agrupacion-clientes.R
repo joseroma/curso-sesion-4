aa50328d370cfa309f2bea62520b96f2f6c03011
Jose <jmaldonado@cash-converters.es>
2019-11-18 21:55:29 +0100

    temp commit clustering

--- 3-clustering/3_2-ejercicio-agrupacion-clientes.R
+++ 3-clustering/3_2-ejercicio-agrupacion-clientes.R
@@ -15,3 +15,18 @@
 ##########################################################
 
 # Cargamos las librerías
+library(tidyverse)
+library(corrplot)
+library(gridExtra)
+library(GGally)
+library(knitr)
+library(dplyr)
+library(factoextra)
+library(cluster)
+set.seed(1234)
+
+##    Leemos los datos
+df <- read.csv("wholesalescustomers.csv")
+
+##    Echamos un vistazo a los datos
+df %>% glimpse()
